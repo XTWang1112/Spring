58d51c39f54d04b0dfade353ca4215e7c691658a
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-12-25 01:15:03 +1100

    PostConstruct annotation

---
+++ Example6/demoproject/src/main/java/com/eazybytes/beans/Vehicle.java
@@ -0,0 +1,28 @@
+package com.eazybytes.beans;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class Vehicle {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @PostConstruct
+    public void initialize(){
+        this.name = "Honda";
+    }
+
+
+    public void printHello(){
+        System.out.println("Printing hello from component vehicle bean");
+    }
+}
---
+++ Example6/demoproject/src/main/java/com/eazybytes/main/Example6.java
@@ -0,0 +1,18 @@
+package com.eazybytes.main;
+
+import com.eazybytes.beans.Vehicle;
+import com.eazybytes.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example6 {
+    public static void main(String[] arg){
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Vehicle vehicle = context.getBean(Vehicle.class);
+
+        System.out.println(vehicle.getName());
+        vehicle.printHello();
+
+
+    }
+}
