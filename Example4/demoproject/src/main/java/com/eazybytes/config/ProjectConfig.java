e08d73be3f6459e6235da6e31891171b670f94af
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-12-25 00:30:31 +1100

    Primary annotation

---
+++ Example4/demoproject/src/main/java/com/eazybytes/config/ProjectConfig.java
@@ -0,0 +1,31 @@
+package com.eazybytes.config;
+
+import com.eazybytes.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ProjectConfig {
+    @Bean(value = "AudiVehicle")
+    Vehicle vehicle1() {
+        var veh = new Vehicle();
+        veh.setName("Audi 8");
+        return veh;
+    }
+
+    @Bean(value = "HondaVehicle")
+    Vehicle vehicle2() {
+        var veh = new Vehicle();
+        veh.setName("Honda");
+        return veh;
+    }
+
+    @Primary
+    @Bean(value = "FerrariVehicle")
+    Vehicle vehicle3() {
+        var veh = new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+}
