1bc4c4e0bf54ffd6384f9b02c0f4100c48b9645a
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-12-26 12:44:38 +1100

    wiring beans using method parameter

---
+++ Example9/demoproject/src/main/java/com/eazybytes/main/Example9.java
@@ -0,0 +1,20 @@
+package com.eazybytes.main;
+
+import com.eazybytes.beans.Person;
+import com.eazybytes.beans.Vehicle;
+import com.eazybytes.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class
+Example9 {
+    public static void main(String[] arg){
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Person person = context.getBean(Person.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+
+        System.out.println("Person name: " + person.getName());
+        System.out.println("Vehicle name: " + vehicle.getName());
+        System.out.println(person.getName() + "'s vehicle: " + person.getVehicle());
+    }
+}
